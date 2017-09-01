db723a9b1076a533736f9f92d0f563a10048ca66
Seb Ashton <seb@sebashton.com>
2017-09-01 15:45:31 +0100

    Refactors

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from collections import OrderedDict
-
 class Tennis:
     def __init__(self, player_one_name, player_two_name):
         self.player_one_name = player_one_name
         self.player_two_name = player_two_name
-        self.players = OrderedDict({player_one_name: 0, player_two_name: 0})
+        self.players = {player_one_name: 0, player_two_name: 0}
         self.score_names = {0: 'love', 1: 'fifteen', 2: 'thirty', 3: 'forty'}
 
     def score(self):
@@ -13,16 +11,19 @@ class Tennis:
                 return 'Deuce'
             else:
                 if self.__scores_are_equal():
-                    return '{0} all'.format(self.__parse_score(self.players[self.player_one_name]))
+                    return '{0} all'.format(self.__parse_score_for(self.player_one_name))
                 else:
                     return '{0} - {1}'.format(
-                        self.__parse_score(self.players[self.player_one_name]),
-                        self.__parse_score(self.players[self.player_two_name]))
+                        self.__parse_score_for(self.player_one_name),
+                        self.__parse_score_for(self.player_two_name))
         except KeyError:
             return self.__special_score()
 
     def won_point(self, player_name):
         self.players[player_name] += 1
+
+    def __parse_score_for(self, player_name):
+        return self.__parse_score(self.players[player_name])
 
     def __parse_score(self, score):
         return self.score_names[score]
