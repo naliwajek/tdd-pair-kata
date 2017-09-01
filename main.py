ecf0b092d2f1bef0422064a02027e76fd1feb57e
Jakub Naliwajek <naliwajek@gmail.com>
2017-09-01 16:06:46 +0100

    Add tennis game

--- main.py
+++ main.py
@@ -1,6 +1,78 @@
-def inc(x):
-    return x + 1
+class PointTranslator:
+    def __init__(self):
+        pass
+
+    def translate(self, point_1, point_2):
+        score_1 = self.__translation_table(point_1)
+        score_2 = self.__translation_table(point_2)
+
+        if point_1 == 0 and point_2 == 0:
+            return 'Love-Love'
+
+        if point_1 == point_2:
+            all_score = score_1 + '-All'
+            if point_1 >= 3:
+                return 'Deuce'
+            else:
+                return all_score
+
+        return score_1 + '-' + score_2
+
+    def __translation_table(self, point):
+        try:
+            return {
+                '0': 'Zero',
+                '1': 'Fifteen',
+                '2': 'Thirty',
+                '3': 'Fourty',
+            }[str(point)]
+        except KeyError:
+            return ''
 
 
-def test_in_a_module():
-    assert inc(7) == 9
+class TennisGame:
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.player_1_score = 0
+        self.player_2_score = 0
+        self.game_over = False
+        self.translator = PointTranslator()
+
+    def score(self):
+        if self.advantage_over(self.player_1_score, self.player_2_score):
+            return 'Advantage for ' + self.player_1
+
+        if self.advantage_over(self.player_2_score, self.player_1_score):
+            return 'Advantage for ' + self.player_2
+
+        if self.player_1_score == 4:
+            self.game_over = True
+            return 'Won for ' + self.player_1
+
+        if self.player_2_score == 4:
+            self.game_over = True
+            return 'Won for ' + self.player_2
+
+        return self.translator.translate(self.player_1_score, self.player_2_score)
+
+    def advantage_over(self, player_with, player_without):
+      return player_with > 3 and (player_without == player_with - 1)
+
+    def won_point(self, player_name):
+        if self.game_over:
+            raise GameOverException
+
+        if player_name == self.player_1:
+            self.player_1_score += 1
+        elif player_name == self.player_2:
+            self.player_2_score += 1
+        else:
+            raise UnknownPlayerException
+
+
+class UnknownPlayerException(Exception):
+    pass
+
+class GameOverException(Exception):
+    pass
--- main_test.py
+++ main_test.py
@@ -1,5 +1,124 @@
+import pytest
 import main
 
 
-def test_answer():
-    assert main.inc(3) == 5
+def test_tennis_game_class():
+    assert main.TennisGame('Alice', 'Bob')
+
+
+def test_player_has_score():
+    t = main.TennisGame('Alice', 'Bob')
+    assert t.score() == 'Love-Love'
+
+
+def test_player_wins_point():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Alice')
+    assert t.score() == 'Fifteen-Zero'
+
+
+def test_player_leads_by_two_points():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Alice')
+    t.won_point('Alice')
+    assert t.score() == 'Thirty-Zero'
+
+
+def test_player_one_wins():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Alice')
+    t.won_point('Alice')
+    t.won_point('Alice')
+    t.won_point('Alice')
+    assert t.score() == 'Won for Alice'
+
+
+def test_player_two_wins():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Bob')
+    t.won_point('Bob')
+    t.won_point('Bob')
+    t.won_point('Bob')
+    assert t.score() == 'Won for Bob'
+
+def the_game_ends():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Bob')
+    t.won_point('Bob')
+    t.won_point('Bob')
+    t.won_point('Bob')
+    with pytest.raises(main.GameOverException):
+        t.won_point('Bob')
+
+def test_unknown_cannot_win_point():
+    t = main.TennisGame('Alice', 'Bob')
+    with pytest.raises(main.UnknownPlayerException):
+        t.won_point('Sally')
+
+
+def test_translator():
+    translator = main.PointTranslator()
+    assert translator.translate(0, 0) == 'Love-Love'
+    assert translator.translate(1, 1) == 'Fifteen-All'
+    assert translator.translate(0, 1) == 'Zero-Fifteen'
+    assert translator.translate(0, 2) == 'Zero-Thirty'
+    assert translator.translate(2, 2) == 'Thirty-All'
+    assert translator.translate(3, 2) == 'Fourty-Thirty'
+    assert translator.translate(3, 3) == 'Deuce'
+    assert translator.translate(4, 4) == 'Deuce'
+    assert translator.translate(14, 14) == 'Deuce'
+
+def test_advantage():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Alice')
+
+    assert t.score() == 'Advantage for Alice'
+
+def won_after_advantage():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Alice')
+    t.won_point('Alice')
+    t.won_point('Alice')
+
+    assert t.score() == 'Won for Alice'
+
+def won_after_advantage_for_second_player():
+    t = main.TennisGame('Alice', 'Bob')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Alice')
+    t.won_point('Bob')
+    t.won_point('Bob')
+
+    assert t.score() == 'Won for Bob'
