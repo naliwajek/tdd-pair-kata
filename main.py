0a82fd5f200d89e16e1498d1fbefd053bd3a3969
Seb Ashton <seb@sebashton.com>
2017-09-01 14:16:31 +0100

    Add won_point score

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ class Tennis:
     def __init__(self, player_one_name, player_two_name):
         self.player_one_name = player_one_name
         self.player_two_name = player_two_name
+        self.player_one_score = 'love'
+        self.player_two_score = 'love'
 
     def score(self):
-        return 'love - love'
+        return self.player_one_score + ' - ' + self.player_two_score
 
     def won_point(self, player_name):
-        return self.score()
+        self.player_one_score = 'fifteen'
